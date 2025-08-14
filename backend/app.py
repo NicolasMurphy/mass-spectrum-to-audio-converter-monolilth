f1f68c1e7a26eae1bd9c090106069bfa428d2325
Nicolas Murphy <murphynick10@gmail.com>
2025-08-13 19:46:53 -0500

    Move rate limiting logic to utils folder

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-import time, base64, os
+import base64, os
 from flask import Flask, request, send_from_directory
 from flask_cors import CORS
 from audio import generate_combined_wav_bytes_and_data
@@ -9,42 +9,11 @@ from db import (
     get_popular_compounds,
     init_pool,
 )
+from utils.rate_limiting import is_rate_limited
 import requests
 import threading
 
 init_pool()
-
-RATE_LIMIT = 20
-WINDOW = 300
-ip_buckets = {}
-
-
-def cleanup_old_buckets():
-    now = time.time()
-    expired_ips = []
-
-    for ip, timestamps in ip_buckets.items():
-        recent_timestamps = [t for t in timestamps if now - t < WINDOW]
-
-        if recent_timestamps:
-            ip_buckets[ip] = recent_timestamps
-        else:
-            expired_ips.append(ip)
-
-    for ip in expired_ips:
-        del ip_buckets[ip]
-
-
-def is_rate_limited(ip):
-    cleanup_old_buckets()
-    now = time.time()
-    if ip not in ip_buckets:
-        ip_buckets[ip] = []
-    ip_buckets[ip] = [t for t in ip_buckets[ip] if now - t < WINDOW]
-    if len(ip_buckets[ip]) >= RATE_LIMIT:
-        return True
-    ip_buckets[ip].append(now)
-    return False
 
 
 app = Flask(__name__)
