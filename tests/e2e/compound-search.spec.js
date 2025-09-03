f6555ce18058336ecf1c93271ad1ff29d58638bc
Nicolas Murphy <murphynick10@gmail.com>
2025-09-03 00:41:38 -0500

    e2e: user can search for caffeine and generate audio

---
+++ tests/e2e/compound-search.spec.js
@@ -0,0 +1,25 @@
+import { test, expect } from '@playwright/test';
+
+test('user can search for caffeine and generate audio', async ({ page }) => {
+  await page.goto('/');
+
+  // manual type and click generate
+  await page.getByRole('textbox', { name: 'Compound Name' }).click();
+  await page.getByRole('textbox', { name: 'Compound Name' }).fill('caffeine');
+  await page.getByRole('button', { name: 'Generate Audio' }).click();
+
+  // table titles are visible
+  await expect(page.getByRole('heading', { name: 'Mass Spectrum Data' })).toBeVisible();
+  await expect(page.getByRole('heading', { name: 'Audio Transformation Data' })).toBeVisible();
+
+  // success message, compound, accession, download button, audio player are visible
+  await expect(page.getByText('Success!')).toBeVisible();
+  await expect(page.getByText('Compound: Caffeine')).toBeVisible();
+  await expect(page.getByText('Accession: MSBNK-ACES_SU-')).toBeVisible();
+  await expect(page.getByRole('link', { name: 'Download WAV' })).toBeVisible();
+  await expect(page.locator('audio')).toBeVisible();
+
+  // piano keys are visible (lowest and highest)
+  await expect(page.getByTestId('container')).toContainText('a');
+  await expect(page.getByTestId('container')).toContainText('k');
+});
